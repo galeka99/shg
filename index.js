4cc5a295fab15cf874d2105e7c8aac346ecafb8a
Galang Ekayudha Permana <galange99@gmail.com>
2020-01-31 20:08:58 +0700

    update typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.hash = require('./modules/hash');
 exports.random = require('./modules/random');
 exports.hmac = require('./modules/hmac');
-exports.ncrypt = require('./modules/encrypt');
+exports.encrypt = require('./modules/encrypt');
 exports.decrypt = require('./modules/decrypt');
 exports.base64 = require('./modules/base64');
